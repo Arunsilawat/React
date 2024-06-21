c640fc4fbc81fc301e221e11374758f47e03903c
Arun silawat <arunsilawat722362@gmail.com>
2024-06-21 16:30:09 +0530

    Table

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,68 @@
+import Employee from "./Employee"
 
+const student=[
+  {
+    "rollno":120,
+    "name":"Arun",
+    "city":"Bhopal"
+  },
+  {
+    "rollno":121,
+    "name":"Rahul",
+    "city":"Betul"
+  },
+  {
+    "rollno":122,
+    "name":"jalaj",
+    "city":"hoshangabad"
+  },
+  {
+    "rollno":123,
+    "name":"subham",
+    "city":"Indore"
+  }
+]
 
-const subject=["java","c++","Python","React","pd","Html"];
-const App=()=>{
-
-   const mysub=subject.map((key)=>{
-    return(
-      <>
-      <li>{key}</li>
-      </>
-    )
-   })
+const ans=student.map((key)=>{
   return(
     <>
-     <ul>{mysub}</ul>
+    <tr>
+      <td>{key.rollno}</td>
+      <td>{key.name}</td>
+      <td>{key.city}</td>
+    </tr>
     </>
-  
   )
+})
+
+//---------------------------------------
+const ans1=Employee.map((key)=>{
+  return(
+    <>
+    <tr>
+      <td>{key.empno}</td>
+      <td>{key.name}</td>
+      <td>{key.salary}</td>
+      <td>{key.city}</td>
+    </tr>
+    </>
+  )
+})
+const App=()=>{
+return(
+<>
+<table border="1" width="500" bgcolor="pink" align="center">
+  <tr bgcolor="orange">
+    <td>Emp num</td>
+    <td>Name</td>
+    <td>Salary</td>
+    <td>City</td>
+  </tr>
+  {ans1}
+</table>
+
+</>
+)
 }
 export default App;
+
---
+++ src/Employee.jsx
@@ -0,0 +1,75 @@
+
+
+
+const Employee=[
+    {
+      "empno":1001,
+      "name":"Arun",
+      "salary":40000,
+      "city":"Bhopal"
+    },
+    {
+        "empno":1001,
+        "name":"Arun",
+        "salary":40000,
+        "city":"Bhopal"
+      },
+      
+      {
+        "empno":1001,
+        "name":"Arun",
+        "salary":40000,
+        "city":"Bhopal"
+      },
+      {
+        "empno":1001,
+        "name":"Arun",
+        "salary":40000,
+        "city":"Bhopal"
+      },
+      {
+        "empno":1001,
+        "name":"Arun",
+        "salary":40000,
+        "city":"Bhopal"
+      },
+      {
+        "empno":1001,
+        "name":"Arun",
+        "salary":40000,
+        "city":"Bhopal"
+      },
+      {
+        "empno":1001,
+        "name":"Arun",
+        "salary":40000,
+        "city":"Bhopal"
+      },
+      {
+        "empno":1001,
+        "name":"Arun",
+        "salary":40000,
+        "city":"Bhopal"
+      },
+      {
+        "empno":1001,
+        "name":"Arun",
+        "salary":40000,
+        "city":"Bhopal"
+      },
+      {
+        "empno":1001,
+        "name":"Arun",
+        "salary":40000,
+        "city":"Bhopal"
+      },
+      {
+        "empno":1001,
+        "name":"Arun",
+        "salary":40000,
+        "city":"Bhopal"
+      }
+      
+  ]
+
+  export default Employee;
