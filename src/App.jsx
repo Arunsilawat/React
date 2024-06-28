3af16100706d3de75fe303e9f651d165739b323a
Arun silawat <arunsilawat722362@gmail.com>
2024-06-28 12:44:28 +0530

    mini dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,212 +1,259 @@
-
-
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+//----------------------- Navbar ---------------------------------------
 import Carousel from 'react-bootstrap/Carousel';
-import Button from 'react-bootstrap/Button';
+/* ---------------------------------carousel end ------------------------------------- */
 import Card from 'react-bootstrap/Card';
-
-// import Container from 'react-bootstrap/Container';
+//--------------------------- card end-----------------------------------
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+//-----------------------------------------------------------------
+// import Button from 'react-bootstrap/Button';
+// import Col from 'react-bootstrap/Col';
+// import Form from 'react-bootstrap/Form';
+// import Row from 'react-bootstrap/Row';
 
-// import { Container,Row,Col} from "react-bootstrap";
-
-
-// const App=()=>{
-//     return(
-
-//         <>
-//         <Container className="text-center border p-3 bg-info">
-//              <h1>Welcome to Cybrom</h1>
-//         </Container>
-//         <Container className="border p-5 text-center">
-//             <Row>
-//                 <Col className="border p-5 bg-info fs-1 text-white"  text-white>
-//                     Hellow we are developer
-//                 </Col>
-//                 <Col className="border bg-danger p-5 fs-1 text-white">
-//                   Welcome to Cybrom
-//                 </Col>
-//             </Row>
-//         </Container>
-
-//         <Container className="bg-info p-4 ">
-//               <Row>
-//                   <Col md={3} className="bg-danger fs-3 text-white border text-center p-5">
-//                      I am from bhopal
-//                   </Col>
-//                   <Col className="bg-success fs-3 text-white border text-center p-5">
-//                      I am from bhopal
-//                   </Col>
-
-//                   <Col className="bg-dark fs-3 text-white border text-center p-5">
-//                      I am from bhopal
-//                   </Col>
-
-//               </Row>
-//         </Container>
-
-        
-//         <Container className="bg-info p-4 ">
-//               <Row>
-//                   <Col md={3} className="bg-danger fs-3 text-white border text-center p-5">
-                     
-//                   </Col>
-//                   <Col className="bg-success fs-3 text-white border text-center p-5">
-                     
-//                   </Col>
-
-//                   <Col className="bg-dark fs-3 text-white border text-center p-5">
-//                      I
-//                   </Col>
-
-//               </Row>
-//               <br />
-//               <Row>
-//                   <Col md={6} className="bg-danger fs-3 text-white border text-center p-5">
-                     
-//                   </Col>
-//                   <Col md={3} className="bg-success fs-3 text-white border text-center p-5">
-                     
-//                   </Col>
-
-//                   <Col md={3} className="bg-dark fs-3 text-white border text-center p-5">
-//                      I
-//                   </Col>
-
-//               </Row>
-//         </Container>
-
-       
-//         </>
-//     )
-// }
-// export default App;
-
-
-//------------------------
-import myimg1 from "./images/r1.jpg";
-import myimg2 from "./images/r2.jpg";
-import myimg3 from "./images/r3.jpg";
 
 
 
 const App=()=>{
-    return(
-        <>
-        {/* <img src={myimg1} width="300" />
-        <img src={myimg2} width="250" />
-        <img src={myimg3} width="250" />
-        <br />
-        <img src="public/a1.jpg" width="300" />
-        <img src="public/a2.jpg" width="300" />
-        <img src="public/a3.jpg" width="300" /> */}
-        <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+  return(
+    <>
+          <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Arun Silawat</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item href="#action5">
+                Something else here
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
           </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-
-<Carousel>
+{/* ----------------------------Navbar end ---------------------------------------- */}
+<Carousel data-bs-theme="dark">
       <Carousel.Item>
-      <img src="public/b1.avif" width="100%" height="600"/>
+      <img src="public/lep1.jpg"  width="100%" height="500"/>
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h5>HP Leptop</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src="public/b2.avif" width="100%" height="600" />
-      <Carousel.Caption>
-          <h3>Second slide label</h3>
+      <img src="public/lep2.jpg"  width="100%" height="500"/>        <Carousel.Caption>
+          <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src="public/b3.avif" width="100%" height="600"/>
+      <img src="public/nav.avif"  width="100%" height="500"/>
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h5>Third slide label</h5>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-<br />
-<br />
+    <br />
+    <br />
+{/* ---------------------------------carousel end ------------------------------------- */}
 
 
-    <Container>
+
+
+    <Container fluid="md">
       <Row>
-        <Col>
-        <Card style={{ width: '18rem' }}>
-<img src="public/b1.avif" width="100%" height="300"/>
-<Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-        </Col>
+      <Col>
+         <Card style={{ width: '18rem' }}>
+         <img src="public/card1.jpg"  width="100%" height="200"/>
+         <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+          Some quick example text to build.
+         </Card.Text>
+         </Card.Body>
+         <Card.Body>
+         <Card.Link href="#">Card Link</Card.Link>
+         <Card.Link href="#">Another Link</Card.Link>
+         </Card.Body>
+         </Card>
+       </Col>
 
-        <Col>
-        <Card style={{ width: '18rem' }}>
-<img src="public/b2.avif" width="100%" height="300"/>
-<Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-        </Col>
+       <Col>
+         <Card style={{ width: '18rem' }}>
+         <img src="public/card2.png"  width="100%" height="200"/>
+         <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+          Some quick example text to build.
+         </Card.Text>
+         </Card.Body>
+         <Card.Body>
+         <Card.Link href="#">Card Link</Card.Link>
+         <Card.Link href="#">Another Link</Card.Link>
+         </Card.Body>
+         </Card>
+       </Col>
 
-        <Col>
-        <Card style={{ width: '18rem' }}>
-<img src="public/b3.avif" width="100%" height="300"/>
-<Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-        </Col>
 
-        
+       <Col>
+         <Card style={{ width: '18rem' }}>
+         <img src="public/card3.webp"  width="100%" height="200"/>
+         <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+          Some quick example text to build.
+         </Card.Text>
+         </Card.Body>
+         <Card.Body>
+         <Card.Link href="#">Card Link</Card.Link>
+         <Card.Link href="#">Another Link</Card.Link>
+         </Card.Body>
+         </Card>
+       </Col>
+  
+       <Col>
+         <Card style={{ width: '18rem' }}>
+         <img src="public/card4.jpeg"  width="100%" height="200"/>
+         <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+          Some quick example text to build.
+         </Card.Text>
+         </Card.Body>
+         <Card.Body>
+         <Card.Link href="#">Card Link</Card.Link>
+         <Card.Link href="#">Another Link</Card.Link>
+         </Card.Body>
+         </Card>
+       </Col>
+
+       <Col>
+         <Card style={{ width: '18rem' }}>
+         <img src="public/card5.jpg"  width="100%" height="200"/>
+         <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+          Some quick example text to build.
+         </Card.Text>
+         </Card.Body>
+         <Card.Body>
+         <Card.Link href="#">Card Link</Card.Link>
+         <Card.Link href="#">Another Link</Card.Link>
+         </Card.Body>
+         </Card>
+       </Col>
+
+       <Col>
+         <Card style={{ width: '18rem' }}>
+         <img src="public/card6.webp"  width="100%" height="200"/>
+         <Card.Body>
+         <Card.Title>Card Title</Card.Title>
+         <Card.Text>
+          Some quick example text to build.
+         </Card.Text>
+         </Card.Body>
+         <Card.Body>
+         <Card.Link href="#">Card Link</Card.Link>
+         <Card.Link href="#">Another Link</Card.Link>
+         </Card.Body>
+         </Card>
+       </Col>
       </Row>
+      
     </Container>
+    <br />
+    <br />
+    {/* --------------------------------card end ----------------------------- */}
 
-        </>
-    )
+     <Container>
+     <Form>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Address 2</Form.Label>
+        <Form.Control placeholder="Apartment, studio, or floor" />
+      </Form.Group>
+
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Select defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+
+     </Container>
+     
+    </>
+  )
 }
-
 export default App;
