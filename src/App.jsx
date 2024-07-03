a49aceb880cd11c842c1feb56362f82fde0c04cd
Arun silawat <arunsilawat722362@gmail.com>
2024-07-03 15:14:05 +0530

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,87 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./Pages/Home";
-import Service from "./Pages/Service";
-import About from "./Pages/About";
-import NoPage from "./Pages/NoPage";
-import Contact from "./Pages/Contact";
-import CompanyService from "./Pages/CompanyService";
-import ProductService from "./Pages/Productservice";
-import SellsService from "./Pages/SellsService";
 
+
+// const App=()=>{
+//   const getval=(e)=>{
+//     let nm=e.target.name;
+//     let val=e.target.value;
+//     alert("Name : " +nm +" Value :"+val)
+//   }
+//    return(
+//       <>
+//         enter name: <input type="text" name="stuname" value="Bhopal" onClick={getval} />
+//       </>
+//    )
+// }
+// export default App;
+//--------------------------------------------
+import { useState } from "react";
+// const App=()=>{
+//    const [name,setName]=useState("Arun");
+//    return(
+//       <>
+//       <h1>Hello We are Cybrom Student : {name}</h1>
+//       <button onClick={()=>{setName("Trun")}}>Click</button>
+//        </>
+//    )
+// }
+// export default App;
+
+// const App=()=>{
+//   const [color,setColor]=useState("Red");
+//   const myColor=()=>{
+//     setColor("Green")
+//   }
+//   return(
+//      <>
+//      <h1>My Favrite Color : {color}</h1>
+//      <button onClick={myColor}>Click</button>
+//       </>
+//   )
+// }
+// export default App;
+//--------------------------------------
+
+// const App=()=>{
+//   const [color,setColor]=useState("Red");
+
+//   return(
+//      <>
+//      <h1 style={{color:color}}>My Favrite Color : {color}</h1>
+//      <button onClick={()=>{setColor("Green")}}>Green</button>
+//      <button onClick={()=>{setColor("black")}}>Black</button>
+//      <button onClick={()=>{setColor("pink")}}>Pink</button>
+//      <button onClick={()=>{setColor("blue")}}>Blue</button>
+//      <button onClick={()=>{setColor("orange")}}>Orange</button>
+
+//       </>
+//   )
+// }
+// export default App;
+
+//-------------------------------------------------
 const App=()=>{
-    return(
-        <>
-          <BrowserRouter>
-             <Routes>
-                  <Route path="/" element={<Layout/>}>
-                       <Route index element={<Home/>}/>
-                       <Route path="/home" element={<Home/>}/>
-                       <Route path="/service" element={<Service/>} >
-                          <Route path="companyservice" element={<CompanyService/>}/>
-                          <Route path="productservice" element={<ProductService/>}/>
-                          <Route path="sellsservice" element={<SellsService/>}/>
-                       </Route>
-                       <Route path="/about" element={<About/>}/>
-                       <Route path="/contact" element={<Contact/>}/>
-                       <Route path="*" element={<NoPage/>}/>
-                  </Route>
-             </Routes>
-          </BrowserRouter>
-        </>
-    )
+  const [cnt,setCnt]=useState(0);
+const myinc=()=>{
+  setCnt(cnt+1)
 }
+const mydec=()=>{
+  setCnt(cnt-1)
+}
+const myrec=()=>{
+  setCnt(0)
+}
+  return(
+     <>
+     <center>
+          <h1>My Count : {cnt}</h1>
+          <button onClick={myinc}>Increment</button>
+          <button onClick={mydec}>Decrement</button>
+          <button onClick={myrec}>Reset</button>
+     </center>
 
+
+
+      </>
+  )
+}
 export default App;
