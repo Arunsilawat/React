98e1779dee1b1c9efe20c94678e71074ffb0e336
Arun silawat <arunsilawat722362@gmail.com>
2024-07-04 16:01:02 +0530

    useState Input

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@
 // }
 // export default App;
 //--------------------------------------------
-import { useState } from "react";
+// import { useState } from "react";
 // const App=()=>{
 //    const [name,setName]=useState("Arun");
 //    return(
@@ -59,29 +59,75 @@ import { useState } from "react";
 // export default App;
 
 //-------------------------------------------------
+// const App=()=>{
+//   const [cnt,setCnt]=useState(0);
+// const myinc=()=>{
+//   setCnt(cnt+1)
+// }
+// const mydec=()=>{
+//   setCnt(cnt-1)
+// }
+// const myrec=()=>{
+//   setCnt(0)
+// }
+//   return(
+//      <>
+//      <center>
+//           <h1>My Count : {cnt}</h1>
+//           <button onClick={myinc}>Increment</button>
+//           <button onClick={mydec}>Decrement</button>
+//           <button onClick={myrec}>Reset</button>
+//      </center>
+
+
+
+//       </>
+//   )
+// }
+// export default App;
+ import { useState } from "react"
+// const App=()=>{
+//   const [name,setName]= useState("");
+//   const [city,setCity]= useState("");
+//   const submitHandle=()=>{
+//     let inputVal={name:name,city:city}
+//     console.log(inputVal);
+//   }
+//   return(
+//     <> 
+//     <h1>I Am Arun </h1>
+//      Enter Name: <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} />
+//      <br />
+//      Enter City: <input type="text" value={city} onChange={(e)=>{setCity(e.target.value)}} />
+//      <br />
+//      <button onClick={submitHandle}>Data Save</button>
+//     </>
+//   )
+// }
+// export default App;
+
 const App=()=>{
-  const [cnt,setCnt]=useState(0);
-const myinc=()=>{
-  setCnt(cnt+1)
-}
-const mydec=()=>{
-  setCnt(cnt-1)
-}
-const myrec=()=>{
-  setCnt(0)
-}
+  const [empno,setEmpno]= useState("");
+  const [name,setName]= useState("");
+  const [city,setCity]= useState("");
+  const [salary,setSalary]= useState("");
+  const submitHandle=()=>{
+    let inputVal={Empno:empno,Name:name,city:city,salary:salary}
+    console.log(inputVal);
+  }
   return(
-     <>
-     <center>
-          <h1>My Count : {cnt}</h1>
-          <button onClick={myinc}>Increment</button>
-          <button onClick={mydec}>Decrement</button>
-          <button onClick={myrec}>Reset</button>
-     </center>
-
-
-
-      </>
+    <> 
+    <h1>I Am Arun </h1>
+     Enter Empno: <input type="text" value={empno} onChange={(e)=>{setEmpno(e.target.value)}} />
+    <br />
+     Enter Name: <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} />
+     <br />
+     Enter City: <input type="text" value={city} onChange={(e)=>{setCity(e.target.value)}} />
+     <br />
+     Enter Salary: <input type="text" value={salary} onChange={(e)=>{setSalary(e.target.value)}} />
+     <br />
+     <button onClick={submitHandle}>Data Save</button>
+    </>
   )
 }
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,4 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+
