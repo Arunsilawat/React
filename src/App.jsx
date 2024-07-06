90eaa35d25715661267c1739e35683d36a5dc21c
Arun silawat <arunsilawat722362@gmail.com>
2024-07-06 15:07:25 +0530

    axios json data fatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,53 @@
-// import { useState } from "react";
-// const App=()=>{
-//   const [input, setInput]=useState({});
-//   const handleInput=(e)=>{
-//        let name=e.target.name;
-//        let value=e.target.value;
-//        setInput(values=>({...values,[name]:value}));
-//        console.log(input);
-//   }
-//   const handlesubmit=()=>{
-//     console.log(input)  
-//   }
-
-import About from "./About";
-
-
-//   return(
-//     <>
-//     Enter Empno  : <input type="text" name="empno" onChange={handleInput}/>
-//     <br />
-//     Enter Name   : <input type="text" name="name" onChange={handleInput}/>
-//     <br />
-//     Enter City   : <input type="text" name="city" onChange={handleInput} />
-//     <br />
-//     Enter Salary : <input type="text" name="salary" onChange={handleInput}/>
-//     <br />
-//     <button onClick={handlesubmit}>Save</button>
-//     </>
-//   )
-// }
-// export default App;
-//----------------------------------------------------------------------------------------today
-// import { useState,useEffect } from "react";
-// const App=()=>{
-//   const [cnt,setCnt]=useState(0);
-//   useEffect(()=>{
-//     setCnt(cnt+1)
-//   },[])
-//   return(
-//     <>
-//       <h1>Application Form :{cnt}</h1>
-//     </>
-   
-//   )
-// }
-// export default App;
-
-
-
 import { useState,useEffect } from "react";
+import axios from "axios";
+
+
 const App=()=>{
-  const [cnt,setCnt]=useState(0);
-  const [multi,setMulti]=useState(1);
-  useEffect(()=>{
-    setMulti(cnt*2)
-  },[cnt])
-  return(
-    <>
-    <button onClick={()=>{setCnt(cnt+1)}}>Click Here</button>
-      <h1>Application Form :{cnt}</h1>
-      <h1>UseEffect :{multi}</h1>
-    </>
-   
-  )
+    const [mydata, setMydata]=useState([]);
+    const loadData=()=>{
+        let url="https://jsonplaceholder.typicode.com/todos/";
+        axios.get(url).then((res)=>{
+        setMydata(res.data)
+        });
+    }
+    useEffect(()=>{
+        loadData();
+    },[])
+    let status="";
+
+    const ans=mydata.map((key)=>{
+        if(key.complated==true){
+            status="True"
+        }
+        else{
+            status="False"
+        }
+        return(
+            <>
+         <tr style={{border:'2px solid black'}}>
+            <td style={{border:'2px solid black'}}>{key.userId}</td>
+            <td style={{border:'2px solid black'}}>{key.id}</td>
+            <td style={{border:'2px solid black'}}>{key.title}</td>
+            <td style={{border:'2px solid black'}}>{status}</td>
+        </tr>
+            </>
+        )
+    })
+
+
+    return(
+        <>
+        <h1>Welcome to Cybrom</h1>
+        <table style={{backgroundColor:'pink',border:'2px solid black'}}>
+            <tr style={{border:'2px solid black'}}>
+            <th style={{border:'2px solid black'}}>User ID</th>
+            <th style={{border:'2px solid black'}}>ID</th>
+            <th style={{border:'2px solid black'}}>Title</th>
+            <th style={{border:'2px solid black'}}>Complated</th>
+            </tr>
+                {ans}
+        </table>
+        </>
+    )
 }
 export default App;
