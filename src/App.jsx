c9f632bd28650c8196dc460e6216e5a4b39a824b
Arun silawat <arunsilawat722362@gmail.com>
2024-07-08 19:35:27 +0530

    create box  with the help or css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState,useEffect } from "react";
 // import axios from "axios";
 
+import axios from "axios";
 import Product from "./Product";
 
 
@@ -54,10 +55,94 @@ import Product from "./Product";
 // }
 // export default App;
 //------------------------------------------------------
+// import { useState,useEffect } from "react";
+// import Table from 'react-bootstrap/Table';
+
+// import axios from "axios";
+// const App=()=>{
+//     const [mydata,setMydata]=useState([]);
+//      const loadData=()=>{
+//        let api= "http://localhost:3000/student";
+//        axios.get(api).then((res)=>{
+//             setMydata(res.data)
+//        });
+//      }
+//      useEffect(()=>{
+//       loadData()
+//      },[])
+
+//     const ans=mydata.map((key)=>{
+//         <tr>
+//             <td>{key.rollno}</td>
+//             <td>{key.name}</td>
+//             <td>{key.city}</td>
+//             <td>{key.fees}</td>
+//         </tr>
+//     })
+//     return(
+//         <>
+//            <table>
+//             <tr>
+//                 <th>Roll No</th>
+//                 <th>Name</th>
+//                 <th>City</th>
+//                 <th>Fees</th>
+//             </tr>
+//              {ans}
+//            </table>
+//         </>
+//     )
+// }
+// export default App;
+
+
+  
+const box1={
+    backgroundColor:'pink',
+    width: '300px',
+    height: '200px',
+    border:'2px solid black',
+    borderRadius:'30px',
+    margin: 'auto',
+   
+    
+ }
+ const box2={
+    backgroundColor:'green',
+    width: '240px',
+    height: '160px',
+    border:'2px solid black',
+    borderRadius:'30px',
+    margin: 'auto',
+     marginTop: 15
+  
+ }
+ const box3={
+    backgroundColor:'yellow',
+    width: '200px',
+    height: '100px',
+    border:'2px solid black',
+    borderRadius:'30px',
+    margin: 'auto',
+     marginTop: 25
+ }
+ const box4={
+    width: '70px',
+    margin: 'auto',
+    marginTop: 27,
+    
+ }
+
 const App=()=>{
     return(
         <>
-        <Product/>
+        <div  style={box1}>
+             <div style={box2}>
+                <div style={box3}>
+                    <div style={box4}>CYBROM</div>
+                </div>
+             </div>
+        </div>
         </>
     )
 }
