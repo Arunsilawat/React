1d0ecd33e48e2bfe9614c570d0f2b3bc8049e28e
Arun silawat <arunsilawat722362@gmail.com>
2024-07-16 20:32:54 +0530

    if else condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./Pages/Home";
-import Display from "./Pages/Display";
-import Contact from "./Pages/Contact";
-import Insert from "./Pages/Insert";
-import Search from "./Pages/Search";
-import Update from "./Pages/Update";
+// import { BrowserRouter,Routes,Route } from "react-router-dom";
+// import Layout from "./Layout";
+// import Home from "./Pages/Home";
+// import Display from "./Pages/Display";
+// import Contact from "./Pages/Contact";
+// import Insert from "./Pages/Insert";
+// import Search from "./Pages/Search";
+// import Update from "./Pages/Update";
 
 
 
+// const App=()=>{
+//     return(
+//         <>
+//         <BrowserRouter>
+//            <Routes>
+//                <Route path="/" element={<Layout/>}>
+//                 <Route index element={<Home/>} />
+//                 <Route path="/home" element={<Home/>} />
+//                 <Route path="/display" element={<Display/>} />
+//                 <Route path="/contact" element={<Contact/>} />
+//                 <Route path="/insert" element={<Insert/>} />
+//                 <Route path="/search" element={<Search/>} />
+//                 <Route path="/update" element={<Update/>}/>
+//                </Route>
+//            </Routes>
+//         </BrowserRouter>
+//         </>
+//     )
+// }
+// export default App;
+// ========================================end=========================================================
+import Collage from "./Collage";
 const App=()=>{
     return(
         <>
-        <BrowserRouter>
-           <Routes>
-               <Route path="/" element={<Layout/>}>
-                <Route index element={<Home/>} />
-                <Route path="/home" element={<Home/>} />
-                <Route path="/display" element={<Display/>} />
-                <Route path="/contact" element={<Contact/>} />
-                <Route path="/insert" element={<Insert/>} />
-                <Route path="/search" element={<Search/>} />
-                <Route path="/update" element={<Update/>}/>
-               </Route>
-           </Routes>
-        </BrowserRouter>
+        <Collage myVal={false}/>
         </>
     )
 }
---
+++ src/Collage.jsx
@@ -0,0 +1,19 @@
+
+
+const Collage=(props)=>{
+    if(props.myVal){
+        return(
+            <>
+            <h1>I am From Bhopal</h1>
+            </>
+        )
+    }
+    else{
+        return(
+            <>
+        <h1>I am Web Developer</h1>            
+            </>
+        )
+    } 
+}
+export default Collage;
