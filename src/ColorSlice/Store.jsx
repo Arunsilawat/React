76452f3a82e4fda327554ba8be4a5da41f6dd949
Arun silawat <arunsilawat722362@gmail.com>
2024-07-31 12:39:53 +0530

    clear

--- src/ColorSlice/Store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import myclr from "./ColoreSlice";
-const Store=configureStore({
-    reducer:{
-        mycolor:myclr
-    }
-})
-export default Store;
--- src/MyStore.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import Result from "./MySlice";
-const MyStore=configureStore({
-    reducer:{
-       mycounter:Result
-    }
-})
-export default MyStore;
