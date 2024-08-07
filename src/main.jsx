d36442c6afd660e183418f88756d341c53d8ecfa
Arun silawat <arunsilawat722362@gmail.com>
2024-08-07 23:01:50 +0530

    React

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,8 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './CSS/style.css'
 import './CSS/Arun.css'
-import Store from './Store.jsx';
-import { Provider } from 'react-redux';
-  ReactDOM.createRoot(document.getElementById('root')).render( 
-
- <Provider store={Store}>
-<App/>
- </Provider>
- )
+    ReactDOM.createRoot(document.getElementById('root')).render( 
+  <App/>
+  )
 
  
