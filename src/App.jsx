419c7de623e8a2db2416cee92b5947048f595634
Arun silawat <arunsilawat722362@gmail.com>
2024-08-16 21:52:55 +0530

    App React

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
- 
- 
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Pages/Home";
+import Shop from "./Pages/Shop";
+import Blog from "./Pages/Blog";
+import Contact from "./Pages/Contact";
+import About from "./Pages/About";
+import CartProduct from "./Pages/CartProduct";
  
  const App=()=>{
    return(
       <>
-      <h1>Cybrom</h1>
+      <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Layout/>}>
+         <Route index element={<Home/>}/>
+         <Route path="home" element={<Home/>}/>
+         <Route path="shop" element={<Shop/>}/>
+         <Route path="blog" element={<Blog/>}/>
+         <Route path="contact" element={<Contact/>}/>
+         <Route path="about" element={<About/>}/>
+         <Route path="cartproduct" element={<CartProduct/>}/>
+         </Route>
+      </Routes>
+      </BrowserRouter>
+      
       </>
    )
 }
