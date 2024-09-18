df54434901a62c7ca54e58b2aa176221a688a9ba
Arun silawat <arunsilawat722362@gmail.com>
2024-09-18 22:44:17 +0530

    Color slice main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './CSS/style.css'
-  ReactDOM.createRoot(document.getElementById('root')).render(
-         <App />
+import Store from './Store.jsx';
+import { Provider } from 'react-redux';
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={Store}>
+            <App />
+    </Provider>
+  
 )
 
