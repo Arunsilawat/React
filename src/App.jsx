0ecf90214c86e965fb400f7437706b2d4dc4edb7
Arun silawat <arunsilawat722362@gmail.com>
2024-09-18 22:32:06 +0530

    Data get

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,50 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./Home";
-import CounterApp from "./CounterApp";
-import ThemeChangeApp from "./ThemeChangeApp";
-import ToDoList from "./ToDoList";
-import Contact from "./Contact";
-
-
-const App=()=>{
-    return(
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Table from 'react-bootstrap/Table';
+const App = () => {
+    const [mydata, setmydata] = useState([]);
+    const datahandle = () => {
+        let api = 'http://localhost:3000/student';
+        axios.get(api).then((res) => {
+            setmydata(res.data)
+        })
+    }
+    useEffect(() => {
+        datahandle();
+    }, [])
+    let sum=0;
+    const ans = mydata.map((key) => {
+        sum+=1
+        return (
+            <>
+                <tr>
+                    <td>{sum}</td>
+                    <td>{key.name}</td>
+                    <td>{key.age}</td>
+                    <td>{key.city}</td>
+                    <td>{key.class}</td>
+                </tr>
+            </>
+        )
+    })
+    return (
         <>
-        <BrowserRouter>
-         <Routes>
-            <Route path="/" element={<Layout/>}>
-            <Route index element={<Home/>}/>
-            <Route path="home" element={<Home/>}/>
-            <Route path="counter" element={<CounterApp/>}/>
-            <Route path="theme" element={<ThemeChangeApp/>}/>
-            <Route path="todo" element={<ToDoList/>}/>
-            <Route path="contact" element={<Contact/>}/>
-            </Route>
-         </Routes>
-        </BrowserRouter>
-        </>
-    )
+            <h1>Welcome to Cybrom</h1>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>S No</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>City</th>
+                        <th>Class</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {ans}
+                </tbody>
+               </Table>
+            </>
+            )
 }
-export default App;
+            export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './CSS/style.css'
-import { Provider } from 'react-redux';
-import Store from './Store.jsx';
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={Store}>
-        <App />
-    </Provider>
-   
-    
+  ReactDOM.createRoot(document.getElementById('root')).render(
+         <App />
 )
 
