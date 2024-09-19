acd6f5a0477a3faae9e11df822558dc628aef88c
Arun silawat <arunsilawat722362@gmail.com>
2024-09-19 23:11:53 +0530

    store

---
+++ color slice/Store.jsx
@@ -0,0 +1,9 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+import myclr from "./ColorSlice"
+const Store=configureStore({
+    reducer:{
+        mycolor:myclr
+    }
+})
+export default Store;
