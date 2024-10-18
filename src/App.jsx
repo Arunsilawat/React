6700bfb485d5e82cb90494b35cb159cf08e4bea9
Arun silawat <arunsilawat722362@gmail.com>
2024-10-18 20:07:05 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
 
+
+
+const App=()=>{
+   return(
+      <>
+      <div style={{width:'400px',height:'300px',border:'2px solid black',overflowX:'hidden',}}>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste autem recusandae officiis placeat, eaque expedita incidunt deserunt ducimus animi! Nemo earum enim, dolor omnis ab quaerat quis optio obcaecati cupiditate.
+         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil iusto odio ipsam quas incidunt obcaecati alias cum animi! Harum nam ipsam quod. Temporibus, soluta quibusdam optio aliquid illum nostrum hic.
+         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae praesentium pariatur autem hic dolorem? Voluptatibus ipsum quibusdam quaerat quasi, sunt suscipit earum ut, libero provident ipsam recusandae similique repellat alias.
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto facere aliquam odit sapiente repellendus, ratione qui! Quae asperiores excepturi animi ab? Non, ad sit incidunt quaerat dolorem asperiores corrupti sapiente!
+      </div>
+      </>
+   )
+}
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './CSS/style.css'
- 
-ReactDOM.createRoot(document.getElementById('root')).render(
-             <App />
- 
-  
-)
+import App from './App';
+import Store from './Store';
+import { Provider } from 'react-redux';
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={Store}>
+          <App />
+    </Provider>
+   
+ )
 
